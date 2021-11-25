2e8a2a29731e5339c5911f2a89ef734d824f1657
Paris Ramírez Saldaña <paris.140815@gmail.com>
2021-11-24 18:50:09 -0600

    Primer commit

---
+++ J-002-Cancha/src/j/pkg002/cancha/J002Cancha.java
@@ -0,0 +1,7 @@
+package j.pkg002.cancha;
+public class J002Cancha {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ J-002-Cancha/src/j/pkg002/cancha/Red.java
@@ -0,0 +1,10 @@
+package j.pkg002.cancha;
+
+public class Red {
+    String [] juega, cieloSi, cieloNo, vientoSi, vientoNo, temperaturaSi, temperaturaNo, humedadSi, humedadNo;
+
+    public Red() {
+        this.juega = new String[]{"Sí", "0.633", "No", "0.367"};
+        
+    }
+}
